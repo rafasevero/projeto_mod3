f82f6e3940ca4cc104b5852da3814aef59d2bdf7
rafasevero <rafasevero2302@gmail.com>
2024-09-13 22:58:41 -0300

    finalizado com instancias manuais e com menus

--- main.php
+++ main.php
@@ -10,11 +10,14 @@ require_once 'vendedor.php';
 require_once 'vender.php';
 require_once 'produto.php';
 
+//instanciando manualmente
 
 // criando humano
+/*
 $humano1 = new Humano("Rafaela Severo",17,"rua dos alfeneiros","40028922");
 $humano2 = new Humano("Louis",22,"rua das galinhas","400232321");
 $humano3 = new Humano("Hugh Jackman",45,"rua dos patinhos","99965743");
+
 
 
 //criando animal
@@ -59,6 +62,111 @@ echo $produto1->listarProduto();
 
 $venda1 = new Venda(26,1,$produto1);
 echo $venda1->carrinho();
+*/
+
+//instanciando por meio de um menu
+
+$humanos = [];
+$animais = [];
+$produtos = [];
+
+do {
+    echo "\nMenu Principal:\n";
+    echo "1. Adicionar Humano\n";
+    echo "2. Adicionar Animal\n";
+    echo "3. Adicionar Produto\n";
+    echo "4. Realizar Venda\n";
+    echo "5. Sair\n";
+    $opcao = readline("Escolha uma opção: ");
+
+    switch ($opcao) {
+        case 1:
+            // Adicionar Humano
+            $nome = readline("Digite o nome do humano: ");
+            $idade = readline("Digite a idade do humano: ");
+            $endereco = readline("Digite o endereço do humano: ");
+            $contato = readline("Digite o contato do humano: ");
+            $novoHumano = new Humano($nome, $idade, $endereco, $contato);
+            $humanos[] = $novoHumano;
+            echo "Humano adicionado: " . $novoHumano->nome . "\n";
+            break;
+
+        case 2:
+            // adicionar Animal
+            $nome = readline("Digite o nome do animal: ");
+            $raca = readline("Digite a raça do animal: ");
+            $quant_pata = readline("Digite a quantidade de patas do animal: ");
+            $cor = readline("Digite a cor do animal: ");
+            $peso = readline("Digite o peso do animal: ");
+            $tamanho = readline("Digite o tamanho do animal (pequeno, médio, grande): ");
+            $nomeHumano = readline("Digite o nome do humano associado: ");
+            
+
+            // encontrar o humano associado
+            $humanoAssociado = null;
+            foreach ($humanos as $humano) {
+                if ($humano->nome == $nomeHumano) {
+                    $humanoAssociado = $humano;
+                    break;
+                }
+            }
+            
+            if ($humanoAssociado) {
+                $novoAnimal = new Animal($nome, $raca, $quant_pata, $cor, $peso, $tamanho, $humanoAssociado);
+                $animais[] = $novoAnimal;
+                echo "Animal adicionado: " . $novoAnimal->nomeAnimal . "\n";
+            } else {
+                echo "Humano associado não encontrado.\n";
+            }
+            break;
+
+        case 3:
+            // adicionar Produto
+            $produtoNome = readline("Digite o nome do produto: ");
+            $valor = readline("Digite o valor do produto: ");
+            $quantidade = readline("Digite a quantidade em estoque: ");
+            $novoProduto = new Produto($produtoNome, $valor, $quantidade);
+            $produtos[] = $novoProduto;
+            echo "Produto adicionado: " . $novoProduto->listarProduto();
+            break;
+
+        case 4:
+            // realizar Venda
+            $nomeProduto = readline("Digite o nome do produto: ");
+            $quantidadeVenda = readline("Digite a quantidade vendida: ");
+            
+            // encontrar o produto
+            $produto = null;
+            foreach ($produtos as $p) {
+                if ($p->produto == $nomeProduto) {
+                    $produto = $p;
+                    break;
+                }
+            }
+            
+            if ($produto) {
+                $valor = $produto->valor * $quantidadeVenda;
+                $novaVenda = new Venda($valor, $quantidadeVenda, $produto);
+                echo "Venda realizada: " . $novaVenda->carrinho();
+            } else {
+                echo "Produto não encontrado.\n";
+            }
+            break;
+
+        case 5:
+            echo "Saindo...\n";
+            break;
+
+        default:
+            echo "Opção inválida. Tente novamente.\n";
+            break;
+    }
+    print_r($humanos);
+    print_r($animais);
+    print_r($produtos);
+    
+
+} while ($opcao != 5);
 
 
 
